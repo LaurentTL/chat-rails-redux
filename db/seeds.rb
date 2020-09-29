63d1ec4fc9afb831e261e128b716f96443b2210e
Laurent Treuil <laurent.treuil@gmail.com>
2020-09-29 09:26:51 +0200

    add nickname to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-require 'facker'
-
 Message.destroy_all
 Channel.destroy_all
 User.destroy_all
 
-names = w%{general react paris}
-nicknames = w%{Laurent Thanh-an Thomas Vorachit}
+names = %w(general react paris)
+nicknames = %w(Laurent Thanh-an Thomas Vorachit)
 
 channels = names.map do |name|
-  Channel.find_or_created_by(name: name)
+  Channel.find_or_create_by(name: name)
 end
 
 users = nicknames.map do |nickname|
@@ -26,3 +24,7 @@ end
   Message.create! user: users.sample, channel: channels.sample, content: Faker::TvShows::HowIMetYourMother.quote
 end
 
+puts 'Channels:'
+channels.each do |channel|
+  puts "- #{channel.id}: #{channel.name}"
+end
